a04b51415249bdc2356b7c92597f2b0972c67c30
Muhammad Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2026-04-28 17:46:24 +0700

    chore: seeding use raw query COPY

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -12,7 +12,6 @@ const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
 
-const BATCH_SIZE = 5_000
 const DATA_DIR = path.resolve('prisma/data')
 const PRODUCTS_CSV = path.join(DATA_DIR, 'amazon_products.csv')
 const CATEGORIES_CSV = path.join(DATA_DIR, 'amazon_categories.csv')
@@ -22,21 +21,6 @@ const RawCategorySchema = z.object({
   category_name: z.string().min(1),
 })
 type RawCategoryType = z.infer<typeof RawCategorySchema>
-
-const RawProductSchema = z.object({
-  asin: z.string().min(1),
-  title: z.string().min(1),
-  imgUrl: z.string(),
-  productURL: z.string(),
-  stars: z.coerce.number(),
-  reviews: z.coerce.number().int(),
-  price: z.coerce.number(),
-  listPrice: z.coerce.number(),
-  category_id: z.coerce.number().int(),
-  isBestSeller: z.coerce.boolean(),
-  boughtInLastMonth: z.coerce.number().int(),
-})
-type RawProductType = z.infer<typeof RawProductSchema>
 
 async function seedCategories(): Promise<void> {
   const start = performance.now()
@@ -77,38 +61,12 @@ async function seedProducts(): Promise<void> {
     return
   }
 
-  console.log(`Streaming Products using batch size ${BATCH_SIZE}`)
-
-  const batch: RawProductType[] = []
-  let totalInserted = 0
-  let batchNum = 0
-
-  const flush = async (rows: RawProductType[]) => {
-    if (rows.length === 0) return
-    batchNum++
-    await prisma.product.createMany({
-      data: rows.map((r) => {
-        const { category_id, productURL, ...rr } = r
-        return { ...rr, categoryId: category_id, productUrl: productURL }
-      }),
-      skipDuplicates: true,
-    })
-    totalInserted += rows.length
-    process.stdout.write(`Batch ${batchNum} | ${totalInserted}\n`)
-  }
-
-  const parser = fs
-    .createReadStream(PRODUCTS_CSV)
-    .pipe(parse({ columns: true, skip_empty_lines: true, trim: true }))
-
-  for await (const record of parser) {
-    const data = RawProductSchema.parse(record)
-    batch.push(data)
-    if (batch.length >= BATCH_SIZE) {
-      await flush(batch.splice(0, BATCH_SIZE))
-    }
-  }
-  await flush(batch)
+  const totalInserted = await prisma.$executeRawUnsafe(`
+  COPY "Product"(asin, title, "imgUrl", "productUrl", stars, reviews, price, "listPrice", "categoryId", "isBestSeller", "boughtInLastMonth")
+  FROM '${PRODUCTS_CSV}'
+  DELIMITER ','
+  CSV HEADER;
+`)
 
   const end = performance.now()
   const duration = ((end - start) / 1000).toFixed(2)
@@ -118,6 +76,9 @@ async function seedProducts(): Promise<void> {
 async function main() {
   await seedCategories()
   await seedProducts()
+
+  const result = await prisma.$queryRaw`SELECT pg_size_pretty(pg_database_size(current_database())) as size` as Record<string, string>[]
+  console.log(`DB size: ${result[0].size}`)
 }
 
 main()
