931fd1cfefafdf699a4207f2896fbaa15c9d94e3
Muhammad Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2026-05-01 15:49:11 +0700

    feat: searchbar navbar ui

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const route = useRoute()
+
 useHead({
   meta: [
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -22,11 +26,45 @@ useSeoMeta({
   ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
   twitterCard: 'summary_large_image',
 })
+
+const { data: categories, status: statusCategories } = useFetch('/api/categories')
+const itemsCategories = computed(() => {
+  if (!categories.value) return []
+  return [
+    { id: -1, name: 'All' },
+    ...categories.value,
+  ]
+})
+const selectedCategories = ref<CategoryType>({ id: -1, name: 'All' })
+
+const itemsNav = computed<NavigationMenuItem[]>(() => [
+  {
+    label: 'Home',
+    to: '/',
+    icon: 'i-lucide-book-open',
+    active: route.path === '/',
+  },
+  {
+    label: 'Catalog',
+    to: '/catalog',
+    icon: 'i-lucide-box',
+    active: route.path.startsWith('/catalog'),
+  },
+])
 </script>
 
 <template>
   <UApp>
-    <UHeader>
+    <UHeader
+      mode="slideover"
+      :ui="{
+        left: 'lg:flex-initial lg:basis-auto',
+        center: 'flex-1 flex items-center justify-center',
+        right: 'lg:flex-initial lg:basis-auto',
+        toggle: 'lg:block',
+        content: 'lg:block',
+      }"
+    >
       <template #left>
         <NuxtLink
           to="/"
@@ -41,11 +79,46 @@ useSeoMeta({
         </NuxtLink>
       </template>
 
-      <!-- TODO: navbar -->
-      <!-- TODO: search bar -->
+      <div class="flex-1 flex w-full max-w-2xl">
+        <UFieldGroup
+          class="w-full"
+        >
+          <USelectMenu
+            v-model="selectedCategories"
+            label-key="name"
+            :items="itemsCategories"
+            :loading="statusCategories === 'pending'"
+            placeholder="Categories"
+            :ui="{ content: 'min-w-fit' }"
+            :content="{
+              align: 'start',
+            }"
+            :search-input="false"
+            color="neutral"
+            size="xl"
+          />
+          <UInput
+            class="w-full"
+            placeholder="Seach AmznPro"
+            size="xl"
+          />
+          <UButton
+            icon="lucide:search"
+            size="xl"
+          />
+        </UFieldGroup>
+      </div>
 
       <template #right>
         <UColorModeButton />
+      </template>
+
+      <template #body>
+        <UNavigationMenu
+          :items="itemsNav"
+          orientation="vertical"
+          class="-mx-2.5"
+        />
       </template>
     </UHeader>
 
---
+++ app/pages/catalog/[[id]].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    Catalog {{ route.params.id ? ' - ' + route.params.id : '' }}
+  </div>
+</template>
